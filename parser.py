f8d8d33e10ce1392fce56d1a61a4b264173c8b00
LAKE <1147359254@qq.com>
2023-10-27 17:21:13 +0800

    [fix] 修改json解析工具

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import autogen
 import os 
 
 
-def _generate_agents(agent_config):
+def _generate_agents(agent_config, llm_config):
 
     agents = dict()
     for agent_data in agent_config['agents']:
@@ -13,12 +13,13 @@ def _generate_agents(agent_config):
                 name=agent_data['name'],
                 system_message=agent_data.get('system_message', None),
                 code_execution_config=agent_data.get('code_execution_config', {}),
-                human_input_mode=agent_data.get('human_input_mode', None)
+                human_input_mode=agent_data.get('human_input_mode', None),
+                max_consecutive_auto_reply=10
             )
         elif agent_type == 'AssistantAgent':
             agent = autogen.AssistantAgent(
                 name=agent_data['name'],
-                system_message=agent_data.get('system_message', None),
+                # system_message=agent_data.get('system_message', None),
                 llm_config=llm_config
             )
         else:
@@ -26,11 +27,14 @@ def _generate_agents(agent_config):
 
         agents[agent_data["name"]] = agent 
 
-        return agents
+    return agents
 
 
 
-def _load_llm_config(file="OAI_CONFIG_LIST"):
+def _load_llm_config(
+    env_or_file="OAI_CONFIG_LIST",
+    file_location=".",
+    filter_dict=None):
     return autogen.config_list_from_json("OAI_CONFIG_LIST")
 
 def _load_agent_config(file):
@@ -45,14 +49,15 @@ def run(msg: str):
     llm_config = _load_llm_config("OAI_CONFIG_LIST")
     agent_config = _load_agent_config("/Users/hupan/Codes/KDF/gitcodes/AI-Agent/configs/no1.json")
     
-    agents = _generate_agents(agent_config)
+    agents = _generate_agents(agent_config, llm_config)
 
-    initiate_config = data.get("start_conversation")
+    initiate_config = agent_config.get("start_conversation")
     initiator = agents[initiate_config.get("initiator")]
     recevior = agents[initiate_config.get("recevior")]
+    
     initiator.initiate_chat(
         recevior,
-        message=msg
+        message=msg    
     )
 
 
